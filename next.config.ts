d8a2371dce13825b8e8acb9a09986d52354976c0
codevatican <unggulsulaiman7@gmail.com>
2026-09-06 01:06:41 +0700

    chore: depedencies + shadcn

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'ojltfmvmbolalhtzrhva.supabase.co', pathname: '/storage/v1/object/public/**' },
+    ],
+  },
 };
+
+
 
 export default nextConfig;
